c388b67a0feecaada6110cb37991e339b21d423d
wulandarykwaitota <93325366+wulandarykwaitota@users.noreply.github.com>
2022-11-18 09:48:17 +0700

    Add files via upload

---
+++ 1_A_71210724.py
@@ -0,0 +1,16 @@
+print("====Test case 1===")
+nama = input ("Nama : ")
+matkul = input ("Masukkan nama matakuliah : ")
+grup = input ("Masukkan grup Anda : ")
+
+print("Haloo! CassandraCassandra")
+print("Anda Tergabung dalam kelas Pratikum Teknologi Komputer A")
+
+print("====Test case 1===")
+nama = input ("Nama : ")
+matkul = input ("Masukkan nama matakuliah : ")
+grup = input ("Masukkan grup Anda : ")
+
+#cetak layar
+print ("Haloo! Alaska")
+print ("Anda Tergabung dalam kelas Pratikum Teknologi Komputer C")
